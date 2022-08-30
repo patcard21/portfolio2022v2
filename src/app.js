8c46d0697e671f45d375cae726e5371f3ad254b0
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-30 15:48:13 -0400

    Updated index.html

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+  template: (
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
+  ),
+});
+
+app.mount('#app')
